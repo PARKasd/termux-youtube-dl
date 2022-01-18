d81966e181df10f30f7c887974b25e35b2040894
0won2 <66772247+PARKasd@users.noreply.github.com>
2022-01-18 20:58:22 +0900

    Update share2youtubedl.sh

--- share2youtubedl.sh
+++ share2youtubedl.sh
@@ -3,18 +3,8 @@ clear
 
 if [[ "$1" =~ ^.*youtu.*$ ]] || [[ "$1" =~ ^.*youtube.*$ ]]; then
     echo -e "Downloading video...\\n>URL: ${1}\\n"
-    yt-dlp -F "$1"
-    read -p "Choose your video quality (press enter for: 'best') : " video
-    read -p "Choose your audio quality (press enter for: 'best') : " audio
-    if [[ "$video" = "" ]]; then
-        video="best"
-    fi
-    if [[ "$audio" = "" ]]; then
-        audio="best"
-    fi
-    yt-dlp -f "$video"+"$audio" "$1"
-    elif [[ "$1" =~ ^.*nourlselected.*$ ]]; then
-    echo "There was an error"
+    yt-dlp -f "best"+"best" "$1"
+    
 else yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' "$1"
 fi
 
