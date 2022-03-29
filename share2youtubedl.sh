70d30d63f088c0191c6ec20ee98e4813d96b54c0
0won2 <66772247+PARKasd@users.noreply.github.com>
2022-03-29 11:55:06 +0900

    Update share2youtubedl.sh

--- share2youtubedl.sh
+++ share2youtubedl.sh
@@ -8,4 +8,3 @@ if [[ "$1" =~ ^.*youtu.*$ ]] || [[ "$1" =~ ^.*youtube.*$ ]]; then
 else yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' "$1"
 fi
 
-read -p "Press enter to continue"
